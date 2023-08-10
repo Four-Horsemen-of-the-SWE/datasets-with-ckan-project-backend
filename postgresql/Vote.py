d267e1e42bae21052d7b3e5b94b3f60a9ef28521
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-08-11 00:42:24 +0700

    feat: enhancement vote system for neutral vote

--- postgresql/Vote.py
+++ postgresql/Vote.py
@@ -13,33 +13,59 @@ class Vote(PostgreSQL):
       self.user = User(jwt_token=jwt_token)
 
   def _vote_type_error(self, vote_type):
-  	if vote_type not in ['upvote', 'downvote']:
+  	if vote_type not in ['upvote', 'downvote', 'neutral']:
   		return True
   	else:
   		return False
 
+  def _is_already_voted(self, target_id, user_id):
+  	with self.engine.connect() as connection:
+  		try:
+	  		query_string = text("SELECT id, target_id, target_type, vote_type, user_id FROM public.vote WHERE target_id = :target_id AND user_id = :user_id")
+	  		result = connection.execute(query_string.bindparams(target_id = target_id, user_id = self.user.id)).mappings().one()
+	  		if result is not None and len(result):
+	  			return {'voted': True, 'id':  result['id'], 'target_id': result['target_id'], 'vote_type': result['vote_type'], 'user_id': result['user_id']}
+	  		else:
+	  			return {'voted': False}
+	  	except NoResultFound:
+	  		return {'voted': False}
+
   # VOTE
   def vote(self, target_id: str = None, target_type: str = None, vote_type: str = None):
-    try:
-      if target_id is None:
-        return {'ok': False, 'message': 'target_id is not provided.'}
-      if target_type is None:
-        return {'ok': False, 'message': 'target_type is not provided.'}
-      if vote_type is None:
-        return {'ok': False, 'message': 'vote_type is not provided.'}
+	  # try:
+	    if target_id is None:
+	      return {'ok': False, 'message': 'target_id is not provided.'}
+	    if target_type is None:
+	      return {'ok': False, 'message': 'target_type is not provided.'}
+	    if vote_type is None:
+	      return {'ok': False, 'message': 'vote_type is not provided.'}
 
-      if self._vote_type_error(vote_type):
-      	return {'ok': False, 'message': 'vote_type not valid.'}
+	    if self._vote_type_error(vote_type):
+	      return {'ok': False, 'message': 'vote_type not valid.'}
 
-      with self.engine.connect() as connection:
-        _id = str(uuid.uuid4())
-        query_string = text("INSERT INTO public.vote( id, target_id, target_type, vote_type, user_id) VALUES (:id, :target_id, :target_type, :vote_type, :user_id)")
-        connection.execute(query_string.bindparams(id=_id, target_id=target_id, target_type=target_type, vote_type=vote_type, user_id=self.user.id))
-        connection.commit()
+	    with self.engine.connect() as connection:
+	      # Check if already voted
+	      result = self._is_already_voted(target_id, self.user.id)
+	      if result['voted']:
+	        if vote_type == result['vote_type']:
+	          return {'ok': False, 'message': 'user already voted'}
+	        else:
+	          query_string = text("UPDATE public.vote SET vote_type = :vote_type WHERE id = :_id")
+	          connection.execute(query_string.bindparams(vote_type=vote_type, _id=result['id']))
+	          connection.commit()
+	          
+	          return {'ok': True, 'message': f'{vote_type} success.', 'vote_type': vote_type}
+	      else:
+	        _id = str(uuid.uuid4())
+	        query_string = text("INSERT INTO public.vote( id, target_id, target_type, vote_type, user_id) VALUES (:id, :target_id, :target_type, :vote_type, :user_id)")
+	        connection.execute(query_string.bindparams(id=_id, target_id=target_id, target_type=target_type, vote_type=vote_type, user_id=self.user.id))
+	        connection.commit()
 
-      return {'ok': True, 'message': f'{vote_type} success.', 'vote_type': vote_type}
-    except:
-      return {'ok': False, 'message': 'backend failed.'}
+	        return {'ok': True, 'message': f'{vote_type} success.', 'vote_type': vote_type}
+	  # except:
+	  #   return {'ok': False, 'message': 'backend failed.'}
+
+
 
   # GET VOTE
   def get_vote(self, target_id: str = None):
@@ -48,10 +74,12 @@ class Vote(PostgreSQL):
         return {'ok': False, 'message': 'target_id is not provided.'}
 
       with self.engine.connect() as connection:
-        query_string = text("SELECT target_id, SUM(CASE WHEN vote_type = 'upvote' THEN 1 WHEN vote_type = 'downvote' THEN -1 ELSE 0 END) AS vote_score FROM public.vote WHERE target_id = :target_id GROUP BY target_id")
+        # query_string = text("SELECT target_id, SUM(CASE WHEN vote_type = 'upvote' THEN 1 WHEN vote_type = 'downvote' THEN -1 ELSE 0 END) AS vote_score FROM public.vote WHERE target_id = :target_id GROUP BY target_id")
+        query_string = text("SELECT target_id, SUM( CASE WHEN vote_type = 'upvote' THEN 1 WHEN vote_type = 'downvote' THEN -1 WHEN vote_type = 'neutral' THEN 0 ELSE 0 END ) AS vote_score FROM public.vote WHERE target_id = :target_id GROUP BY target_id")
         result = connection.execute(query_string.bindparams(target_id=target_id)).mappings().one()
         vote_result = []
         return {'ok': True, 'message': 'success', 'vote': result['vote_score']}
-
+    except NoResultFound:
+    	return {'ok': True, 'message': 'success', 'vote': 0}
     except:
-      return {'ok': False, 'message': 'backend failed.'}
+    	return {'ok': False, 'message': 'backend failed.'}
