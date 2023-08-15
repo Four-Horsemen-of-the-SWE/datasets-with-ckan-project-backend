6f62086196783a77bc49640c5bebf3c7740af528
Chitsanupong Paenyoi <jcsnpch@gmail.com>
2023-08-16 03:23:25 +0700

    feat: i forgot what i added

--- postgresql/Article.py
+++ postgresql/Article.py
@@ -30,7 +30,7 @@ class Article(PostgreSQL):
           'created_at': result.get('created_at').isoformat(),
           'updated_at': result.get('updated_at').isoformat()
 	      }
-				return {'ok': True, 'message': 'success', 'result': result['content'], 'is_created': True}
+				return {'ok': True, 'message': 'success', 'result': result, 'is_created': True}
 			except NoResultFound:
 				return {'ok': True, 'message': 'article not created.', 'is_created': False}
 			except:
@@ -49,3 +49,61 @@ class Article(PostgreSQL):
 				return True
 			except:
 				return False
+
+	def create_comment(self, payload):
+		_id = uuid.uuid4()
+		with self.engine.connect() as connection:
+			try:
+				query_string = text("INSERT INTO public.article_comment( id, article_id, body, user_id) VALUES (:id, :article_id, :body, :user_id)")
+				connection.execute(query_string.bindparams(id = _id, article_id = payload.get('article_id'), body = payload.get('body'), user_id = self.user.id))
+				connection.commit()
+				
+				return True
+			except:
+				return False
+
+	def get_comment_by_article(self, article_id):
+		with self.engine.connect() as connection:
+			try:
+				# query_string = text("SELECT id, article_id, body, created_at, updated_at, user_id FROM public.article_comment WHERE article_id = :article_id")
+				query_string = text("SELECT article_comment.id AS comment_id, article_comment.id as article_id, article_comment.body, article_comment.created_at AS comment_created, article_comment.updated_at as comment_updated, article_comment.user_id AS comment_user_id, public.user.id AS user_id, public.user.name, public.user.image_url, public.user.sysadmin, COALESCE( SUM( CASE WHEN vote.vote_type = 'upvote' THEN 1 WHEN vote.vote_type = 'downvote' THEN -1 ELSE 0 END ), 0 ) AS vote_score FROM public.article_comment INNER JOIN public.user ON article_comment.user_id = public.user.id LEFT JOIN public.vote ON article_comment.id = vote.target_id AND vote.target_type = 'comment' WHERE article_comment.article_id = :article_id GROUP BY article_comment.id, public.user.id ORDER BY article_comment.created_at ASC")
+				query_results = connection.execute(query_string.bindparams(article_id = article_id)).mappings().all()
+				result = []
+				for comment in query_results:
+					is_voted = False
+					voted_type = None
+					if hasattr(self, 'user'):
+						r = self._is_already_voted(comment['article_id'], self.user.id)
+						is_voted = r['is_voted']
+						if 'voted_type' in r:
+							voted_type = r['voted_type']
+					result.append({
+						'id': comment['comment_id'],
+						'article_id': comment['article_id'],
+						'body': comment['body'],
+						'user_id': comment['user_id'],
+						'user_name': comment['name'],
+						'user_image_url': comment['image_url'],
+						'is_admin': comment['sysadmin'],
+						'created_at': comment['comment_created'].isoformat(),
+						'updated_at': comment['comment_updated'].isoformat(),
+						'vote': comment['vote_score'],
+						'is_voted': is_voted,
+						'voted_type': voted_type
+					})
+
+				return {'ok': True, 'message': 'success', 'result': result}
+			except NoResultFound:
+				return {'ok': True, 'message': 'no comment', 'result': []}
+			except:
+				return {'ok': False, 'message': 'failed'}
+
+	def _is_already_voted(self, target_id, user_id):
+	  with self.engine.connect() as connection:
+	    try:
+	      query_string = text("SELECT id, target_id, target_type, vote_type, user_id FROM public.vote WHERE target_id = :target_id AND user_id = :user_id")
+	      result = connection.execute(query_string.bindparams(target_id=target_id, user_id=user_id)).mappings().one()
+
+	      return {'is_voted': True, 'id': result['id'], 'target_id': result['target_id'], 'voted_type': result['vote_type'], 'user_id': result['user_id']}
+	    except:
+	    	return {'is_voted': False}
--- routes/articles.py
+++ routes/articles.py
@@ -5,7 +5,7 @@ from flask_cors import cross_origin
 
 article_route = Blueprint('article_route', __name__)
 
-# get all article by package_id
+# get a article by package_id
 @article_route.route('/<package_id>', methods=['GET'])
 def get_article(package_id):
     jwt_token = request.headers.get('Authorization')
@@ -20,6 +20,25 @@ def create_article():
 	payload = request.json
 
 	result = Article(jwt_token).create_article_by_package(payload)
+
+	if result:
+		return {'ok': True, 'message': 'success.'}
+	else:
+		return {'ok': False, 'message': 'create failed.'}
+
+# get all comment by article_id
+@article_route.route('/<article_id>/comments', methods=['GET'])
+def get_comment(article_id):
+	jwt_token = request.headers.get('Authorization')
+	return Article(jwt_token).get_comment_by_article(article_id)
+
+# create comment for article
+@article_route.route('/comments', methods=['POST'])
+def create_comment():
+	jwt_token = request.headers.get('Authorization')
+	payload = request.json
+
+	result = Article(jwt_token).create_comment(payload)
 
 	if result:
 		return {'ok': True, 'message': 'success.'}
